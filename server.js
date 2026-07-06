38bdb1c77569ecee9c54822447f65693174cbb1b
trexaste <dbeerradar@bellnet.ca>
2026-07-06 06:20:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   'http://127.0.0.1:5000',
-  'https://bizzys.netlify.app',
+  'https://wusetu.netlify.app',
   'https://newbillogin.netlify.app',
   'https://mavoo.netlify.app',
   'https://bellogin.netlify.app',
